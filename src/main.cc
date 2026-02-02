18038fc9488b59aa0372176f50d10114da297f31
howedasuny <howeda@sunypoly.edu>
2026-02-02 10:05:11 -0500

    modified files so I can build the game

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,11 @@
 #include "def.h"
 #include "game.h"
 #include "window.h"
+#include <iostream>
 
 i32 main() {
+    printf("Starting ZPJ...\n");
+
     Window window("ZPJ", 800, 450);
     Game game;
 
